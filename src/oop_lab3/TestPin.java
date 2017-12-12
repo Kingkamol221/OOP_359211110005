645a8b90ac73aaee9905aa5fa456233110ae070f
LAB_COM-MT_1 <Kingkamol2048@gmail.com>
2017-12-12 11:41:41 +0700

    TestPin

--- src/oop_lab3/TestPin.java
+++ src/oop_lab3/TestPin.java
@@ -1,4 +1,28 @@
 package oop_lab3;
 
+
+import java.util.Scanner;
+
 public class TestPin {
-}
+    public static void main (String [] ages){
+        String PIN = "1234",input;
+        int count = 0;
+        Scanner scanner =new Scanner(System.in);
+        do{
+            System.out.print("Enter PIN: ");
+            input = scanner.nextLine();
+            if (PIN.equals(input)){
+                System.out.println("Welcame.");
+                break;
+            }else {
+                count++;
+                if (count==3){
+                    System.out.println("Locked");
+                    break;
+                }
+            }
+        }while (count <= 3);
+
+
+    }
+}//cass
