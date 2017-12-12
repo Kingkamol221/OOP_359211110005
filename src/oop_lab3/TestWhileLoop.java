11b7b41612c9aeee0ffc22b004ae8de965b1e47c
LAB_COM-MT_1 <Kingkamol2048@gmail.com>
2017-12-12 10:41:14 +0700

    LoopFor

---
+++ src/oop_lab3/TestWhileLoop.java
@@ -0,0 +1,19 @@
+package oop_lab3;
+
+public class TestWhileLoop {
+    public static void main (String[] args){
+//  while
+//    int i =1;
+//    while (i<=10){
+//        System.out.print(i+" ");
+ //       i++;
+
+        int i =1;
+        while (i<=50){
+            System.out.print(i+" ");
+            if (i%10==0)
+                System.out.print("Hello");
+            i++;
+    }
+    }
+}
