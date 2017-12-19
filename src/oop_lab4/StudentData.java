b5c91ae73a9dd441d7437ee469222eddef3523b6
LAB_COM-MT_1 <Kingkamol2048@gmail.com>
2017-12-19 10:27:25 +0700

    OverLoadMethods

--- src/oop_lab4/StudentData.java
+++ src/oop_lab4/StudentData.java
@@ -6,9 +6,44 @@ package oop_lab4;
 //4.สาขาและมหาวิทยาลัย
 //5.email
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class StudentData {
-    public static void  main (String[] arge){
-        
+    public static BufferedReader reader= new BufferedReader(
+            new InputStreamReader(System.in));
+    public static String getname() throws IOException{
+        System.out.println("Enter your name");
+        return reader.readLine();
+    }//getname
+    public static String getaddress() throws IOException{
+        System.out.println("Enter your address");
+        return reader.readLine();
+    }//getaddress
+    public static String getid() throws IOException{
+        System.out.println("Enter your id");
+        return reader.readLine();
+    }//getid
+    public static String getfaculty() throws IOException{
+        System.out.println("Enter your faculty");
+        return reader.readLine();
+    }//getfaculty
+    public static String getaemail() throws IOException{
+        System.out.println("Enter your email");
+        return reader.readLine();
+    }//getemail
+
+    public static void  main (String[] args) throws IOException{
+    String name = getname();
+    String address =   getaddress();
+    String id = getid();
+    String faculty = getfaculty();
+    String email = getaemail();
+    System.out.println(name+"\n"+address+"\n"+id+"\n"+faculty+"\n"+email);
+    System.out.println(getname()+"\n"+getaddress());
+
+
     }
 }
 
