64be4594b2270bf6174bcfe8c32807cabe024d3f
LAB_COM-MT_1 <Kingkamol2048@gmail.com>
2018-01-30 10:31:57 +0700

    Employee

--- src/oop_lab8/PersonInfo.java
+++ src/oop_lab8/PersonInfo.java
@@ -1,4 +1,31 @@
 package oop_lab8;
 
 public class PersonInfo {
+    public static void main (String[] args){
+        Student student = new Student(
+                 "1249900364241",
+                 "Kingkamol Janmanit",
+                 "fermale",
+                 22,
+                 "359211110005",
+                "IS Management Technology");
+        System.out.println(student.toString());
+
+        System.out.println(student.getName());
+    }
+
+    {
+        Employee employee = new Employee(
+                "25535544",
+                "pojatkan",
+                "30000");
+        System.out.println(employee.toString());
+        System.out.println(employee.getEmployeeID());
+
+
+
+
+    }
+
+
 }
